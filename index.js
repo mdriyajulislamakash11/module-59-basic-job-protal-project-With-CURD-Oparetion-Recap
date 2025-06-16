9c0f78e41d2298e6dd61048bd8c25e0f169a3645
Akash <hafejmohammadakash@gmail.com>
2025-06-16 18:33:29 +0600

    het oparetion

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const jobsCollections = client.db("job-hunter").collection("jobs");
+
+    app.get("/jobs", async (req, res) => {
+        const cursor = jobsCollections.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
